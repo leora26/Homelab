c9fcd6ad444f6d64d34b5f2106cb84d217188783
Leonid  Romaniukha <leonid.romaniukha@student.kdg.be>
2026-01-09 23:11:13 +0100

    Refactoring #2: moved definition of proto files to a separate folder. Build of proto files is succesfull

---
+++ backend/homelab_back/libs/proto/build.rs
@@ -0,0 +1,32 @@
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+
+    println!("cargo:rerun-if-changed=proto/white_listed_user.proto");
+    println!("cargo:rerun-if-changed=proto/user.proto");
+    println!("cargo:rerun-if-changed=proto/file.proto");
+    println!("cargo:rerun-if-changed=proto/global_file.proto");
+    println!("cargo:rerun-if-changed=proto/folder.proto");
+    println!("cargo:rerun-if-changed=proto/label.proto");
+    println!("cargo:rerun-if-changed=proto/file_label.proto");
+    println!("cargo:rerun-if-changed=proto/types.proto");
+
+    tonic_build::configure()
+        .compile_protos(
+            &[
+                "proto/white_listed_user.proto",
+                "proto/user.proto",
+                "proto/file.proto",
+                "proto/global_file.proto",
+                "proto/folder.proto",
+                "proto/label.proto",
+                "proto/file_label.proto",
+                "proto/types.proto",
+            ],
+            &["proto"],
+        )?;
+
+    Ok(())
+}
---
+++ backend/homelab_back/libs/proto/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod common {
+    tonic::include_proto!("common");
+}
+
+pub mod nas {
+    tonic::include_proto!("nas");
+}
+
+pub mod user {
+    tonic::include_proto!("user");
+}
